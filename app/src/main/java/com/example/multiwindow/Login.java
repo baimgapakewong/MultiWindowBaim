bd4a2a975bd07c451d9a4b2b8d5631e20362f5ca
baimgapakewong <marsbrohim26@gmail.com>
2022-10-19 11:35:00 +0700

    BaimMultiwindow

---
+++ app/src/main/java/com/example/multiwindow/Login.java
@@ -0,0 +1,26 @@
+package com.example.multiwindow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, MainActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/multiwindow/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.multiwindow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnberita = findViewById(R.id.btn_berita);
+        Button btnprofile = findViewById(R.id.btn_profile);
+        Button btnexit = findViewById(R.id.btn_exit);
+
+        btnberita.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, berita.class));
+            }
+        });
+        btnprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, profile.class));
+            }
+        });
+        btnexit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, NewActivity.class));
+            }
+        });
+
+    }
+}
